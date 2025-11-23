e621a91c27107f29afcdf9f72bd3943a973f9188
Bidisha Biswas <bdsh.14@gmail.com>
2025-11-23 13:26:13 -0700

    Initial Commit

---
+++ StressNudger/StressNudgerApp.swift
@@ -0,0 +1,17 @@
+//
+//  StressNudgerApp.swift
+//  StressNudger
+//
+//  Created by Bidisha Biswas on 11/23/25.
+//
+
+import SwiftUI
+
+@main
+struct StressNudgerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
